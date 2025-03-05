a36e85a773dcd307949f5da51d82e1df5cefd9e5
Marek Balvín <marek267balvin@gmail.com>
2025-03-05 19:20:04 +0100

    Added embedded apps

---
+++ src/routes/langtons-ant/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Window from '$lib/components/window/Window.svelte';
+</script>
+
+<Window title="Langton's Ant" icon="executable">
+	<embed
+		src="https://marekblvn.github.io/langtons-ant/"
+		width="100%"
+		height="100%"
+		type="text/html"
+	/>
+</Window>
