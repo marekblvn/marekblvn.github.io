03a3722c6f7606921f10b1b7a7f50d3ae6046714
Marek Balvín <marek267balvin@gmail.com>
2025-03-06 17:54:29 +0100

    Prepared route structure

--- src/routes/about-me/+page.svelte
+++ src/routes/about-me/+page.svelte
@@ -1,7 +1,29 @@
 <script>
+	import { onMount } from 'svelte';
 	import Toolbar from '$lib/components/toolbar/Toolbar.svelte';
 	import Window from '$lib/components/window/Window.svelte';
 	import windowIcon from '$lib/static/icons/user-documents.ico';
+	import { marked } from 'marked';
+
+	let markdownContent = $state('');
+	onMount(() => {
+		import('$lib/static/markdown/ABOUTME.md?raw').then(async (module) => {
+			markdownContent = await marked(module.default);
+		});
+	});
 </script>
 
-<Window title="About Me" icon={windowIcon} toolbar={Toolbar} />
+<Window title="About Me" icon={windowIcon} toolbar={Toolbar}>
+	<div class="markdown">
+		{@html markdownContent}
+	</div>
+</Window>
+
+<style>
+	.markdown {
+		margin: auto;
+		padding: 12px;
+		font-size: 14px;
+		letter-spacing: 0.3px;
+	}
+</style>
