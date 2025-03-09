7e283d35a6ccf39dabacb22491bcfa1a8881d0cc
Marek Balvín <marek267balvin@gmail.com>
2025-03-09 03:18:51 +0100

    Created help window

---
+++ src/routes/help/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+	import Window from '$lib/components/window/Window.svelte';
+	import windowIcon from '$lib/static/icons/help.ico';
+</script>
+
+<Window title="Help" icon={windowIcon}></Window>
