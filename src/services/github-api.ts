c732260be7b42e5a3402f6a1fc843453b2764ebd
Marek Balvín <marek267balvin@gmail.com>
2025-03-21 14:53:35 +0100

    Added github api service

---
+++ src/services/github-api.ts
@@ -0,0 +1,13 @@
+import axios, { AxiosResponse } from "axios";
+
+const apiInstance = axios.create({
+  baseURL: "https://api.github.com/",
+  responseType: "json",
+});
+
+type GetUserPublicReposParams = { username: string };
+export async function getUserPublicRepos({
+  username,
+}: GetUserPublicReposParams): Promise<AxiosResponse<Record<string, unknown>>> {
+  return await apiInstance.get(`/users/${username}/repos`);
+}
