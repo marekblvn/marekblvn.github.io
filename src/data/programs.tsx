16ccda862f7ae0acacdea93c997a78644b547cfa
Marek Balvín <marek267balvin@gmail.com>
2025-03-26 13:26:23 +0100

    Added embeds

--- src/data/programs.tsx
+++ src/data/programs.tsx
@@ -1,12 +1,18 @@
 import { ReactElement } from "react";
 import Projects from "../programs/projects";
+import GameOfLife from "../programs/game-of-life";
+import LangtonsAnt from "../programs/langtons-ant";
 import { v4 as uuidv4 } from "uuid";
+import { IconCode } from "../components/window-control-button/WindowControlButton";
 
 export interface Program {
   label: string;
   icon: string;
   component: ReactElement;
   code: string;
+  controls: Array<IconCode>;
+  fullScreenOnly: boolean;
+  resizable: boolean;
 }
 
 export default [
@@ -15,17 +21,26 @@ export default [
     icon: "folder",
     component: <Projects />,
     code: uuidv4(),
+    controls: ["minimize", "maximize", "close"],
+    fullScreenOnly: false,
+    resizable: true,
   },
   {
     label: "Game of Life",
     icon: "executable",
-    component: <Projects />,
+    component: <GameOfLife />,
     code: uuidv4(),
+    controls: ["minimize", "close"],
+    fullScreenOnly: true,
+    resizable: false,
   },
   {
     label: "Langton's Ant",
     icon: "executable",
-    component: <Projects />,
+    component: <LangtonsAnt />,
+    controls: ["minimize", "close"],
     code: uuidv4(),
+    fullScreenOnly: true,
+    resizable: false,
   },
 ] as Array<Program>;
